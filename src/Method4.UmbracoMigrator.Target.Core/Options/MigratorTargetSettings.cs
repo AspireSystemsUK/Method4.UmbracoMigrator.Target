937e96d0affe0b5d9b100c9027920766f5026c7b
Owain Jones <owain.jones@method4.co.uk>
2024-12-31 17:38:22 +0000

    Moved the migration lookups db calls into their own repository class. (#7)
    Added a new class that extends MigrationLookupsRepository, which will store migration relations into the Umbraco Runtime Cache.
    
    Added a new app setting to disable this feature.

--- src/Method4.UmbracoMigrator.Target.Core/Options/MigratorTargetSettings.cs
+++ src/Method4.UmbracoMigrator.Target.Core/Options/MigratorTargetSettings.cs
@@ -1,8 +1,28 @@
-﻿namespace Method4.UmbracoMigrator.Target.Core.Options
+﻿using System.ComponentModel;
+
+namespace Method4.UmbracoMigrator.Target.Core.Options
 {
     public class MigratorTargetSettings
     {
-        public bool EnableMediaRedirectGeneration { get; set; }
+        /// <summary>
+        /// For use with the Skybrud Redirects package.
+        /// Enables the UniqueMediaPathScheme IMediaPathScheme.
+        /// This will automatically generate a re-direct if a migrated media item's file is changed.
+        /// Please see the documentation for more information.
+        /// </summary>
+        [DefaultValue(false)]
+        public bool EnableMediaRedirectGeneration { get; set; } = false;
+
+        /// <summary>
+        /// The property alias of the Media File Upload property.
+        /// </summary>
+        [DefaultValue("umbracoFile")]
         public string? MediaFileUploadPropertyAlias { get; set; }
+
+        /// <summary>
+        /// Enable the Runtime Caching on the MigrationLookups repository
+        /// </summary>
+        [DefaultValue(true)]
+        public bool EnableMigrationLookupTableRuntimeCaching { get; set; } = true;
     }
 }
---
+++ src/Method4.UmbracoMigrator.Target.Core/Repositories/IMigrationLookupsRepository.cs
@@ -0,0 +1,11 @@
+﻿using Method4.UmbracoMigrator.Target.Core.Models.DataModels;
+
+namespace Method4.UmbracoMigrator.Target.Core.Repositories;
+
+public interface IMigrationLookupsRepository
+{
+    NodeRelation? Get(string columnName, string value);
+    void Insert(string newId, string oldId, string newKey, string oldKey);
+    int Count();
+    void DeleteAll();
+}
