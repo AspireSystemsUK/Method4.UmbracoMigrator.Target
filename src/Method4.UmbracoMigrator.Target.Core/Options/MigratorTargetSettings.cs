e55b21d2d7fc3d33b0c7b304c4bf3c783ad3ce56
Carwyn-Phillips <57954098+Carwyn-Phillips@users.noreply.github.com>
2024-12-31 17:24:39 +0000

    Change MigratedUrlRedirectMediaPathScheme to not in use by default (#6)
    * Only enable the url redirect path scheme if it's enabled in the app settings to allow for projects to configure their own
    
    * Update to reduce the usage of magic strings in the composer.
    
    ---------
    
    Co-authored-by: Owain Jones <owaingijones@outlook.com>

--- src/Method4.UmbracoMigrator.Target.Core/Options/MigratorTargetSettings.cs
+++ src/Method4.UmbracoMigrator.Target.Core/Options/MigratorTargetSettings.cs
@@ -3,6 +3,6 @@
     public class MigratorTargetSettings
     {
         public bool EnableMediaRedirectGeneration { get; set; }
-        public string MediaFileUploadPropertyAlias { get; set; }
+        public string? MediaFileUploadPropertyAlias { get; set; }
     }
 }
